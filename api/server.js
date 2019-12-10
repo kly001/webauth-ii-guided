9a73c7532e66ee3dcd2d38ad3ba27760d7bf555f
kly001 <karenly1961@gmail.com>
2019-12-10 13:32:51 -0500

    Initialized; edits to server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+const session = require('express-session');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
@@ -5,11 +6,24 @@ const cors = require('cors');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
+const sessionOptions = {
+  name: "boogie",
+  secret: "damianisthesecret",
+  cookie: {
+    maxAge: 1000*60*60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false
+}
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionOptions))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
